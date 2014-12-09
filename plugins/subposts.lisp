e1d8fcfc85cce5af16b6076ef0809876ceacfc72
Colin M. Strickland <cms@beatworm.co.uk>
2014-12-09 10:05:14 +0000

    Minimum implementation of subclass for posts
    hacky override of discover post method, still needs work

--- plugins/subposts.lisp
+++ plugins/subposts.lisp
@@ -4,23 +4,14 @@
   (:import-from :coleslaw
                 #:post
                 #:discover
-                #:slugify
-                #:render
                 #:find-all
                 #:publish
                 #:add-document
-                #:theme-fn
-                #:render-text
-                #:add-injection
-                #:*injections*
                 #:*config*
                 #:read-content
                 #:construct
-                #:compute-url
-                #:author
                 #:repo
                 #:file
-                #:make-keyword
                 #:do-files
                 #:write-document))
 
@@ -30,19 +21,18 @@
   ((type :initarg :type :reader post-type))
   (:default-initargs :type "standard-post" :title "title"))
 
-(defmethod initialize-instance :after ((object subpost) &key)
-  (with-slots (type)
-      (setf type "standard type")))
-
 (defmethod publish ((doc-type (eql (find-class 'subpost))))
   (dolist (subpost (find-all 'subpost))
     (write-document subpost)))
+
+
+(defmethod discover ((doc-type (eql (find-class 'post))))
+  (format t "Ignoring type :post overridden by subpost plugin~%"))
 
 (defmethod discover ((doc-type (eql (find-class 'subpost))))
   (let ((file-type (format nil "~(~A~)" 'post)))
     (do-files (file (repo *config*) file-type)
       (let ((obj (construct 'subpost (read-content file))))
-        (print-object obj *standard-output*)
         (add-document obj)))))
 
 
